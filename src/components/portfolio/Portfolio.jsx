39eb1ab96b7ebd24b481e7d038e56d74bbee042e
omarelnahs <omarelnahas1141@gmail.com>
2024-02-01 00:09:25 +0200

    سش

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './portfolio.css'
+import Menu from './Menu'
 
 const Portfolio = () => {
+  const [items, setItems] = useState(Menu)
+  const filterItem = (categoryItem) =>{
+    const updateItems = Menu.filter((curElment) =>{
+      return curElment.category === categoryItem
+    })
+
+    setItems((updateItems))
+  }
+
   return (
-    <div>Portfolio</div>
+    <section className="work container section" id='portfolio'> 
+      <h2 className="section__title">Recent Works</h2>
+
+      <div className="work__filters">
+        <span className="work__item" onClick={() => setItems(Menu)}>EveryThing</span>
+        <span className="work__item" onClick={() => filterItem("Creative")}>Creative</span>
+        <span className="work__item" onClick={() => filterItem("Art")}>Art</span>
+        <span className="work__item" onClick={() => filterItem("Design")}>Design</span>
+      </div>
+
+      <div className="work__container grid">
+        {items.map((element) => {
+          const {id, image, title, category, link} = element
+          return(
+            <div className='work__card' key={id}>
+              <div className="work__thumbnail">
+                <img src={image} alt="" className='work__img' />
+                <div className="work__mask"></div>
+              </div>
+
+              <span className="work__category">{category}</span>
+              <h3 className="work__title">{title}</h3>
+              <a href={link} className="work__button">
+                <i className="icon-link work__button-icon"></i>
+              </a>
+            </div>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
